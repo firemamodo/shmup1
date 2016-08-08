96043c35eb2e942aa06d4ce6362880f5345a123e
firemamodo <firemamodo@gmail.com>
2016-08-07 18:25:01 -0700

    movement

--- NateDustinShmup/Assets/Scripts/Player.cs
+++ NateDustinShmup/Assets/Scripts/Player.cs
@@ -1,22 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     public float vertSpeed;
     public float horzSpeed;
+    float speedMultiplier=25;
+   // public Vector3 moveV3 = new Vector3(0, 0, 0);
 
+    private 
 	// Use this for initialization
 	void Start () {
-	
-	}
+
+
+
+
+    }
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 
-        vertSpeed =  Input.GetAxis("verticalLS");
-        horzSpeed = Input.GetAxis("horizontalLS");
+        vertSpeed =  Input.GetAxis("vertical_LS");
+        horzSpeed = Input.GetAxis("horizontal_LS");
 
         Debug.Log("Vert speed is " + vertSpeed + " Horz speed is " + horzSpeed);
+
+        transform.Translate( new Vector3(-horzSpeed, 0, vertSpeed) * speedMultiplier* Time.deltaTime);
     }
 }
